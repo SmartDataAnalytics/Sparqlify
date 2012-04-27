fd3cc59eba221c99e291b9be532aad9917bdf27f
cstadler <cstadler@informatik.uni-leipzig.de>
2012-04-27 15:04:05 +0200

    Fixed a bug in the BSBM mapping

--- run-sparqlify-bsbm.sh
+++ run-sparqlify-bsbm.sh
@@ -1,1 +1,1 @@
-java -cp target/sparqlify-0.0.1-SNAPSHOT-jar-with-dependencies.jar org.aksw.sparqlify.rest.Main -h localhost -u postgres -p postgres -d bsbm1 -c examples/Bsbm.sparqlify -t 30 -n 1000
+java -cp target/sparqlify-0.0.1-SNAPSHOT-jar-with-dependencies.jar org.aksw.sparqlify.rest.Main -h localhost -u postgres -p postgres -d bsbm1 -c mappings/Bsbm.sparqlify -t 30 -n 1000
