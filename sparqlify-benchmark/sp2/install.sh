e8d65aec1e2bef4a787babd21f365270fe80f793
Jörg Unbehauen <joerg.unbehauen@gmail.com>
2012-09-20 19:58:26 +0200

    some sp2 mapping changes

--- sparqlify-benchmark/sp2/install.sh
+++ sparqlify-benchmark/sp2/install.sh
@@ -11,3 +11,16 @@ rm sp2b.tar.gz
 # Copy the run-sparqlify-sp2.sh script and make it executable
 cp -ui run-sparqlify-sp2.sh.dist run-sparqlify-sp2.sh
 chmod +x run-sparqlify-sp2.sh
+
+
+# do the same for d2r
+rm d2rq-0.8.1.tar.gz
+wget https://github.com/downloads/d2rq/d2rq/d2rq-0.8.1.tar.gz
+tar -zxvf d2rq-0.8.1.tar.gz
+mv  d2rq-0.8.1/* d2r
+rmdir d2rq-0.8.1
+rm d2rq-0.8.1.tar.gz
+
+
+cp -ui run-d2r-sp2.sh.dist run-d2r-sp2.sh
+chmod +x run-d2r-sp2.sh
