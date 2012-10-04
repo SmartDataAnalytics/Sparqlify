531b4329f81480be62fc5bc86c90a4c2ff2caf04
cstadler <cstadler@informatik.uni-leipzig.de>
2012-10-04 02:14:49 +0200

    Slowly getting the new infrastructure done

---
+++ sparqlify-core/src/main/java/org/aksw/sparqlify/core/sparql/ResultSetFactory.java
@@ -0,0 +1,61 @@
+package org.aksw.sparqlify.core.sparql;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.aksw.sparqlify.core.ResultSetSparqlify;
+import org.aksw.sparqlify.core.domain.RestrictedExpr;
+
+import com.google.common.collect.Multimap;
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.sparql.core.Var;
+import com.hp.hpl.jena.sparql.engine.binding.Binding;
+
+public class ResultSetFactory {
+
+	/**
+	 * 
+	 * @param conn
+	 * @param sqlQuery
+	 * @param sparqlVarMap
+	 * @param projectionVars The variables that should appear in the result set. If null, sparqlVarMap.keySet() is used.
+	 * @return
+	 * @throws SQLException
+	 */
+	public static ResultSetSparqlify create(Statement stmt, String sqlQuery, Multimap<Var, RestrictedExpr> sparqlVarMap, List<Var> projectionVars)
+		throws SQLException
+	{
+		List<String> resultVars = new ArrayList<String>();
+
+		if(projectionVars == null) {
+			projectionVars = new ArrayList<Var>();
+			for(Node var : sparqlVarMap.keySet()) {
+				projectionVars.add((Var)var);
+			}
+		}
+		
+		for(Var var : projectionVars) {
+			resultVars.add(var.getName());
+		}
+		
+		
+		
+		// TODO ONLY FOR USE WITH MYSQL - MAY HAVE UNDEFINED EFFECTS WITH OTHER DBSs
+		// Enables streaming result sets
+		/*
+		stmt.setFetchSize(Integer.MIN_VALUE);
+		*/
+		
+		ResultSet rs = stmt.executeQuery(sqlQuery);
+		Iterator<Binding> it = new IteratorResultSetSparqlifyBinding(rs, sparqlVarMap);
+
+		ResultSetSparqlify result = new ResultSetSparqlify(it, resultVars, 0);
+		
+		return result;
+	}
+
+}
