cd304c6e080ee37e892e317662e8534719fdb851
cstadler <cstadler@informatik.uni-leipzig.de>
2013-02-16 22:25:24 +0100

    Some deleted files

---
+++ sparqlify-core/run-dump-panlex-links.sh
@@ -0,0 +1,2 @@
+java -cp target/sparqlify-core-jar-with-dependencies.jar org.aksw.sparqlify.web.Main -h localhost -u postgres -p postgres -d panlex  -t 10 -c /home/raven/Projects/Eclipse/panlex-llod/src/plx-links.sparqlify -D
+
