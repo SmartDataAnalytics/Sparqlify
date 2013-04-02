1f1203c5525147c23b9ed32048114c8010b087c4
Sören Brunk <soeren.brunk@tu-dresden.de>
2013-04-02 16:14:03 +0200

    change WKTLiteral -> wktLiteral to conform to final GeoSPARQL spec

--- sparqlify-core/src/main/java/org/aksw/sparqlify/core/Vocab.java
+++ sparqlify-core/src/main/java/org/aksw/sparqlify/core/Vocab.java
@@ -3,6 +3,6 @@ package org.aksw.sparqlify.core;
 
 // TODO Move to a more appropriate package
 public class Vocab {
-	public static final String wktLiteral = "http://www.opengis.net/ont/geosparql#WKTLiteral";
+	public static final String wktLiteral = "http://www.opengis.net/ont/geosparql#wktLiteral";
 	
 }
