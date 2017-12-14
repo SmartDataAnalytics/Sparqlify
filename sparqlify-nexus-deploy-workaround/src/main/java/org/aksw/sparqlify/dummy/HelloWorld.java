ddfd565cf1dd2a2482b072f9b3226654e8ff415b
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-12-14 20:21:23 +0100

    workaround added

--- sparqlify-nexus-deploy-workaround/src/main/java/org/aksw/sparqlify/dummy/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-
-public class MainDummy {
-    public static void main(String[] args) {
-    }
-}
-
---
+++ sparqlify-nexus-deploy-workaround/src/main/java/org/aksw/sparqlify/dummy/MainHelloWorld.java
@@ -0,0 +1,8 @@
+package org.aksw.sparqlify.dummy;
+
+public class MainHelloWorld {
+    public static void main(String[] args) {
+    	System.out.println("Hello World!");
+    }
+}
+
